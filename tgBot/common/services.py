c031d8346ddc347afa9b36409326f1e1f98907ce
Funny Development <ya.giggs2013@yandex.ru>
2024-01-02 04:18:16 +0100

    init movie url attribute and added new expeption

--- tgBot/common/services.py
+++ tgBot/common/services.py
@@ -8,10 +8,11 @@ logger = logging.getLogger(__name__)
 class MovieAPIClient:
     http = HTTPRequestMaker()
 
-    def __init__(self, api_url: str, api_key: str, api_token: str):
+    def __init__(self, api_url: str, api_key: str, api_token: str, movie_url: str):
         self.api_url = api_url
         self.api_key = api_key
         self.api_token = api_token
+        self.movie_url = movie_url
 
     async def fetch_movie(self, query: str) -> dict:
         url = f"{self.api_url}"
@@ -21,18 +22,20 @@ class MovieAPIClient:
         }
         response = await self.http.get(url=url, params=params)
         parsed_data = await self.data_parse(response["results"])
-        print(parsed_data)
         return parsed_data
 
-    @staticmethod
-    async def data_parse(url_response) -> dict:
+    async def data_parse(self, url_response) -> dict | None:
         parsed_data = dict()
         try:
-            parsed_data["id"] = url_response[0]["id"]
             parsed_data["title"] = url_response[0]["original_title"]
             parsed_data["overview"] = url_response[0]["overview"]
             parsed_data["poster"] = url_response[0]["poster_path"]
             parsed_data["rating"] = url_response[0]["vote_average"]
+            parsed_data["movie_link"] = f'{self.movie_url}{url_response[0]["id"]}'
+        except IndexError as error:
+            logger.error("Can't retrieve first element from result: %s", error)
+            return
         except KeyError as error:
             logger.error("Can't retrieve first element from result: %s", error)
+            return
         return parsed_data
