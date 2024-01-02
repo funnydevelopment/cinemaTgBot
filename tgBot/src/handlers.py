11f29554fa368a136fbc843d4cb1ea69f1f1a1df
Funny Development <ya.giggs2013@yandex.ru>
2024-01-02 04:19:24 +0100

    created search movie handler

--- tgBot/src/handlers.py
+++ tgBot/src/handlers.py
@@ -4,6 +4,7 @@ from aiogram import Router, types, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
+from common.create_bot import api_client
 from .states import CinemaBotState
 from . import text_messages as texts
 from . import keyboards as kb
@@ -35,7 +36,7 @@ async def stats_command(message: types.Message, state: FSMContext):
     user_name, telegram_user_id = message.from_user.full_name, message.from_user.id
     await message.answer(
         text=texts.STATS_TEXT.format(user_name=user_name),
-        reply_markup=kb.make_main_menu_kb()
+        reply_markup=kb.make_main_menu_kb(),
     )
     await state.set_state(state=CinemaBotState.main_state)
 
@@ -45,7 +46,7 @@ async def history_command(message: types.Message, state: FSMContext):
     user_name, telegram_user_id = message.from_user.full_name, message.from_user.id
     await message.answer(
         text=texts.HISTORY_TEXT.format(user_name=user_name),
-        reply_markup=kb.make_main_menu_kb()
+        reply_markup=kb.make_main_menu_kb(),
     )
     await state.set_state(state=CinemaBotState.main_state)
 
@@ -66,3 +67,25 @@ async def statistic_command(message: types.Message, state: FSMContext):
 async def story_command(message: types.Message, state: FSMContext):
     await history_command(message, state)
     await state.set_state(state=CinemaBotState.main_state)
+
+
+@router.message()
+async def search_movie(message: types.Message, state: FSMContext):
+    movie_name = message.text
+    search_result = await api_client.fetch_movie(query=movie_name)
+    if search_result:
+        await message.answer(
+            text=texts.SEARCH_RESULT_TEXT.format(
+                name=search_result["title"],
+                rating=search_result["rating"],
+                overview=search_result["overview"],
+                link=search_result["movie_link"],
+            ),
+            reply_markup=kb.make_main_menu_kb(),
+        )
+        await state.set_state(state=CinemaBotState.main_state)
+    else:
+        await message.answer(
+            text=texts.FAILED_RESULT_TEXT, reply_markup=kb.make_main_menu_kb()
+        )
+        await state.set_state(state=CinemaBotState.main_state)
